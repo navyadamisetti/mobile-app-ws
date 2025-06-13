14f1a3503d7286c4d2be7197ce306b9e609f9692
Navya Damisetti <navyaratna.deepika14@gmail.com>
2025-06-12 23:55:35 -0400

    test dependencies added

---
+++ src/test/java/com/appsdeveloperblog/app/ws/ui/topic/TopicControllerTest.java
@@ -0,0 +1,15 @@
+package com.appsdeveloperblog.app.ws.ui.topic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TopicControllerTest {
+
+	@Test
+	void test() {
+		System.out.println("This test ran");
+//		fail("Not yet implemented");
+	}
+
+}
